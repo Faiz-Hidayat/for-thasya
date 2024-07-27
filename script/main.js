13d0bcfc5e4f45d82dce0e8d0aae1d90f8b48264
Faiz-Hidayat <109057000+Faiz-Hidayat@users.noreply.github.com>
2024-07-27 16:56:41 +0700

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,300 @@
+// trigger to play music in the background with sweetalert
+window.addEventListener('load', () => {
+  Swal.fire({
+    title: 'Mau pake lagu?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Ya',
+    cancelButtonText: 'Nggk',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const audio = document.querySelector('.song');
+      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      const source = audioContext.createMediaElementSource(audio);
+      const gainNode = audioContext.createGain();
+      const biquadFilter = audioContext.createBiquadFilter();
+
+      // Mengatur filter untuk merendahkan nada awal
+      biquadFilter.type = 'lowshelf';
+      biquadFilter.frequency.setValueAtTime(1000, audioContext.currentTime); // Frekuensi cutoff
+      biquadFilter.gain.setValueAtTime(-30, audioContext.currentTime); // Gain untuk merendahkan nada
+
+      source.connect(biquadFilter);
+      biquadFilter.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+
+      // Mengatur waktu mulai audio ke 60 detik (1 menit)
+      audio.currentTime = 105;
+      audio.play();
+
+      // Mengembalikan nada ke normal perlahan setelah 5 detik
+      const transitionDuration = 13; // Durasi transisi dalam detik
+      setTimeout(() => {
+        const currentTime = audioContext.currentTime;
+        biquadFilter.gain.linearRampToValueAtTime(0, currentTime + transitionDuration);
+      }, 13000);
+
+      animationTimeline();
+    } else {
+      animationTimeline();
+    }
+  });
+});
+
+// animation timeline
+const animationTimeline = () => {
+  // split chars that needs to be animated individually
+
+  const hbd = document.getElementsByClassName('wish-hbd')[0];
+
+  hbd.innerHTML = `<span>${hbd.innerHTML.split('').join('</span><span>')}</span>`;
+
+  const ideaTextTrans = {
+    opacity: 0,
+    y: -20,
+    rotationX: 5,
+    skewX: '15deg',
+  };
+
+  const ideaTextTransLeave = {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: '-15deg',
+  };
+
+  // timeline
+  const tl = new TimelineMax();
+
+  tl.to('.container', 0.6, {
+    visibility: 'visible',
+  })
+    .from('.one', 0.7, {
+      opacity: 0,
+      y: 10,
+    })
+    .from(
+      '.two',
+      0.4,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      '+=3.5'
+    )
+    .to(
+      '.one',
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      '+=1.5'
+    )
+    .to(
+      '.two',
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      '-=1'
+    )
+    .from('.three', 0.7, {
+      opacity: 0,
+      y: 10,
+    })
+    .to(
+      '.three',
+      0.7,
+      {
+        opacity: 0,
+        y: 10,
+      },
+      '+=3'
+    )
+    .from('.fake-btn', 0.3, {
+      scale: 0.2,
+      opacity: 0,
+    })
+    .to(
+      '.fake-btn',
+      0.1,
+      {
+        backgroundColor: 'rgb(127, 206, 248)',
+      },
+      '+=3'
+    )
+    .from('.idea-1', 0.7, ideaTextTrans)
+    .to('.idea-1', 0.7, ideaTextTransLeave, '+=2.5')
+    .from('.idea-2', 0.7, ideaTextTrans)
+    .to('.idea-2', 0.7, ideaTextTransLeave, '+=2.5')
+    .from('.idea-3', 0.7, ideaTextTrans)
+    .to('.idea-3 strong', 0.5, {
+      scale: 1.2,
+      x: 10,
+      backgroundColor: 'rgb(21, 161, 237)',
+      color: '#fff',
+    })
+    .to('.idea-3', 0.7, ideaTextTransLeave, '+=2.5')
+    .from('.idea-4', 0.7, ideaTextTrans)
+    .to('.idea-4', 0.7, ideaTextTransLeave, '+=2.5')
+    .from(
+      '.idea-5',
+      0.7,
+      {
+        rotationX: 15,
+        rotationZ: -10,
+        skewY: '-5deg',
+        y: 50,
+        z: 10,
+        opacity: 0,
+      },
+      '+=1.5'
+    )
+    .to(
+      '.idea-5 span',
+      0.7,
+      {
+        rotation: 90,
+        x: 8,
+      },
+      '+=1.4'
+    )
+    .to(
+      '.idea-5',
+      0.7,
+      {
+        scale: 0.2,
+        opacity: 0,
+      },
+      '+=2'
+    )
+    .staggerFrom(
+      '.idea-6 span',
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: 15,
+        ease: Expo.easeOut,
+      },
+      0.2
+    )
+    .staggerTo(
+      '.idea-6 span',
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: -15,
+        ease: Expo.easeOut,
+      },
+      0.2,
+      '+=1.5'
+    )
+    .staggerFromTo(
+      '.baloons img',
+      2.5,
+      {
+        opacity: 0.9,
+        y: 1400,
+      },
+      {
+        opacity: 1,
+        y: -1000,
+      },
+      0.2
+    )
+    .from(
+      '.profile-picture',
+      0.5,
+      {
+        scale: 3.5,
+        opacity: 0,
+        x: 25,
+        y: -25,
+        rotationZ: -45,
+      },
+      '-=2'
+    )
+    .from('.hat', 0.5, {
+      x: -100,
+      y: 350,
+      rotation: -180,
+      opacity: 0,
+    })
+    .staggerFrom(
+      '.wish-hbd span',
+      0.7,
+      {
+        opacity: 0,
+        y: -50,
+        // scale: 0.3,
+        rotation: 150,
+        skewX: '30deg',
+        ease: Elastic.easeOut.config(1, 0.5),
+      },
+      0.1
+    )
+    .staggerFromTo(
+      '.wish-hbd span',
+      0.7,
+      {
+        scale: 1.4,
+        rotationY: 150,
+      },
+      {
+        scale: 1,
+        rotationY: 0,
+        color: '#ff69b4',
+        ease: Expo.easeOut,
+      },
+      0.1,
+      'party'
+    )
+    .from(
+      '.wish h5',
+      0.5,
+      {
+        opacity: 0,
+        y: 10,
+        skewX: '-15deg',
+      },
+      'party'
+    )
+    .staggerTo(
+      '.eight svg',
+      1.5,
+      {
+        visibility: 'visible',
+        opacity: 0,
+        scale: 80,
+        repeat: 3,
+        repeatDelay: 1.4,
+      },
+      0.3
+    )
+    .to('.six', 0.5, {
+      opacity: 0,
+      y: 30,
+      zIndex: '-1',
+    })
+    .staggerFrom('.nine p', 1, ideaTextTrans, 1.2)
+    .to(
+      '.last-smile',
+      0.5,
+      {
+        rotation: 90,
+      },
+      '+=1'
+    );
+
+  // Restart Animation on click
+  const replyBtn = document.getElementById('replay');
+  replyBtn.addEventListener('click', () => {
+    tl.restart();
+  });
+};
